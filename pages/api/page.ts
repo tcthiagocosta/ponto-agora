2edd402cda420657ae99994dc88b4d8654753a1a
Thiago Costa <tcostao@indracompany.com>
2021-01-12 14:24:51 -0300

    Ajuste agente page

--- pages/api/page.ts
+++ pages/api/page.ts
@@ -43,6 +43,7 @@ async function getScreenshot() {
   const browser = await puppeteer.launch(options);
 
   const page = await browser.newPage();
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36');
 
   await page.goto('https://webponto.norber.com.br/webPontoIndra/default.asp');
   await page.type('#CodEmpresa', '1')
